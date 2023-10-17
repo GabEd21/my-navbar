7c539619835e8337a6b0502e3464946bd3e19466
GabEd21 <cegdumandan@gmail.com>
2023-10-17 19:19:00 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Component1 from './components/Component1'
+import UserProvider from './context/UserProvider'
+
+function App() {
+  
+  return (
+    <UserProvider>
+    <Component1></Component1>
+    </UserProvider>
+  )
+}
+
+export default App
